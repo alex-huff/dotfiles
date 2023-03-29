281d53decbd726347c77b1597c171ae62ab9e861
alex-huff <alexanderhuff53@gmail.com>
2023-03-28 20:52:29 -0500

    Changed font to RecMonoCasual and added script to change font

---
+++ change-font.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+from='JetBrainsMono Nerd Font'
+to='RecMonoCasual'
+
+find dotfiles -type f -exec file {} + | awk -F: '/ASCII text/ { print $1 }
+                                                 /JSON text data/ { print $1 }
+                                                 /Unicode text, UTF-8 text/ { print $1 }' | \
+xargs -d '\n' sed -i "s/$from/$to/g"
