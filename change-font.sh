2c157d585882e7df72981497e9202aa7322455f1
alex-huff <alexanderhuff53@gmail.com>
2023-04-06 19:04:09 -0500

    Switched to VictorMono for cryllic support

--- change-font.sh
+++ change-font.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-from='Recursive Monospace Casual'
-to='RecMonoCasual Nerd Font'
+from='RecMonoCasual Nerd Font'
+to='VictorMono Nerd Font Mono'
 
 find dotfiles -type f -exec file {} + | awk -F: '/ASCII text/ { print $1 }
                                                  /JSON text data/ { print $1 }
