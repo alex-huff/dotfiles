c2cf81f38c2c7306c107d919c01c0a714a52d301
alex-huff <alexhuff@bakahuff.com>
2023-08-29 15:15:05 -0500

    Renamed mod-menu-integration -> mc-cli

--- dotfiles/.local/bin/open-mc-mod-configuration.sh
+++ dotfiles/.local/bin/open-mc-mod-configuration.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pid=$(focused-pid.sh)
-mod=$(mod-menu-integration --pid $pid get-mod-names | rofi -dmenu -i -theme ~/.config/rofi/launchers/type-1/style-5.rasi)
-mod-menu-integration --pid $pid open-config "$mod"
+mod=$(mc-cli --pid $pid get-mod-names | rofi -dmenu -i -theme ~/.config/rofi/launchers/type-1/style-5.rasi)
+mc-cli --pid $pid open-config "$mod"
