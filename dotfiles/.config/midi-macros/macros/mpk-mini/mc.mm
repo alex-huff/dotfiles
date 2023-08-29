3585b038ac97409cef36d8702a8a602611b05624
alex-huff <alexhuff@bakahuff.com>
2023-08-29 13:01:40 -0500

    Refactored midi-macros

--- dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
+++ dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
@@ -4,13 +4,5 @@
 	mod=$(mod-menu-integration --pid $pid get-mod-names | rofi -dmenu -i -theme ~/.config/rofi/launchers/type-1/style-5.rasi)
 	mod-menu-integration --pid $pid open-config "$mod"
 }
-37{c==9} →
-{
-	pid=$(focused-pid.sh)
-	mod-menu-integration --pid $pid open-config Tweakeroo
-}
-38{c==9} →
-{
-	pid=$(focused-pid.sh)
-	mod-menu-integration --pid $pid open-config MiniHUD
-}
+37{c==9} → mod-menu-integration --pid $(focused-pid.sh) open-config Tweakeroo
+38{c==9} → mod-menu-integration --pid $(focused-pid.sh) open-config MiniHUD
