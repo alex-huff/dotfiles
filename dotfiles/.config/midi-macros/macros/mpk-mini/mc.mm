293c3f5de6127ea9f422fd697e512f43f8d49d9c
alex-huff <alexhuff@bakahuff.com>
2023-09-06 15:56:51 -0500

    Added message while changing fov

--- dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
+++ dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
@@ -1,4 +1,8 @@
 36{c==9} → open-mc-mod-configuration.sh
 37{c==9} → mc-cli --pid $(focused-pid.sh) open-config Tweakeroo
 38{c==9} → mc-cli --pid $(focused-pid.sh) open-config MiniHUD
-D3 MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"→f"{round(CC_VALUE_SCALED(30, 110))}") → mc-cli --pid $(focused-pid.sh) set-fov {}
+D3 MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"→f"{round(CC_VALUE_SCALED(30, 110))}") [BLOCK|DEBOUNCE]→
+{
+	pid=$(focused-pid.sh)
+	mc-cli --pid $pid send chat-local "$(mc-cli --pid $pid set-fov {})"
+}
