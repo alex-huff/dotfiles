cd09e965b250b099633a6cad37202724e601bf3a
alex-huff <alexhuff@bakahuff.com>
2023-09-06 14:01:41 -0500

    Updated mc-cli

--- dotfiles/.local/bin/open-mc-mod-configuration.sh
+++ dotfiles/.local/bin/open-mc-mod-configuration.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pid=$(focused-pid.sh)
-mod=$(mc-cli --pid $pid get-mod-names | rofi -dmenu -i -theme ~/.config/rofi/launchers/type-1/style-5.rasi)
+mod=$(mc-cli --pid $pid get-config-names | rofi -dmenu -i -theme ~/.config/rofi/launchers/type-1/style-5.rasi)
 mc-cli --pid $pid open-config "$mod"
