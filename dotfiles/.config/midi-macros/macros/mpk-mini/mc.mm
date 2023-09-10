c3fa0b4c435e7c10b04b4bf0cb3b14759bc94beb
alex-huff <alexhuff@bakahuff.com>
2023-09-10 12:23:11 -0500

    Added brightness control for mc

--- dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
+++ dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
@@ -6,3 +6,8 @@ D3 MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"→f"{round(CC_VALUE_SCALED(30, 110))}"
 	pid=$(focused-pid.sh)
 	mc-cli --pid $pid send chat-local "$(mc-cli --pid $pid set-fov {})"
 }
+D3+E3 MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"→f"{CC_VALUE_SCALED(0, 1)}") [BLOCK|DEBOUNCE]→
+{
+	pid=$(focused-pid.sh)
+	mc-cli --pid $pid send chat-local "$(mc-cli --pid $pid set-brightness {})"
+}
