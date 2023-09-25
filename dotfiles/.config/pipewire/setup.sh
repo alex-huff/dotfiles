fce65c68937ca1d1071e7e14235eeca49dd0fde0
alex-huff <alexanderhuff53@gmail.com>
2023-09-25 10:37:35 -0500

    Fixed pipewire setup

--- dotfiles/.config/pipewire/setup.sh
+++ dotfiles/.config/pipewire/setup.sh
@@ -3,6 +3,8 @@
 default_sink=$(pactl get-default-sink)
 pactl load-module module-null-sink sink_name=hear
 pactl load-module module-null-sink sink_name=hush
-pactl load-module module-loopback source=hear sink=$default_sink
-pactl load-module module-loopback source=hush sink=$default_sink
-pactl set-default-sink hear
+hear_sink=$(pactl list sinks short | grep hear | cut -f 1)
+hush_sink=$(pactl list sinks short | grep hush | cut -f 1)
+pactl load-module module-loopback source=$hear_sink sink=$default_sink
+pactl load-module module-loopback source=$hush_sink sink=$default_sink
+pactl set-default-sink $hear_sink
