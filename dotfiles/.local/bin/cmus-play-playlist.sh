13ce3e28bb18377fadc375a04772e0229a48abad
alex-huff <alexhuff@bakahuff.com>
2023-10-16 19:54:43 -0500

    Combined multiple cmus commands into one cmus-remote command where possible

--- dotfiles/.local/bin/cmus-play-playlist.sh
+++ dotfiles/.local/bin/cmus-play-playlist.sh
@@ -14,31 +14,18 @@ identifier=ba304144-e8ad-42c2-9975-93e2c6d0ee56
 # save vol_left and vol_right
 read -d "\n" left right < <(cmus-remote -Q | grep --extended-regexp "vol_(left|right)" | cut -d " " -f 3)
 
-# go to playlist view
-cmus-remote -C "view 3"
-
-# go to top of window
-cmus-remote -C win-top
-
-# mute
-cmus-remote -C "vol 0"
-
-# activate either 000 playlist or top song in current playlist
-cmus-remote -C win-activate
+# go to playlist view, got to top of current window, mute and activate selected item
+cmus-remote -C "view 3" win-top "vol 0" win-activate
 
 # identify whether we are in left or right window
+# this works since:
+# if we were in left window, we are now playing the only song in the top playlist, which is named $identifier
+# if we were in the right window, we are now playing the top song in our current playlist, which is not named $identifier
 if ! cmus-remote -Q | grep $identifier &> /dev/null
 then
 	# we are in right window, move to top of left
-	cmus-remote -C win-next
-	cmus-remote -C win-top
+	cmus-remote -C win-next win-top
 fi
 
-# move to selected playlist
-cmus-remote -C "win-down $(($playlist_position - 1))"
-
-# play playlist
-cmus-remote -C win-activate
-
-# restore volume
-cmus-remote -C "vol ${left}% ${right}%"
+# move to selected playlist, activate it, and restore volume
+cmus-remote -C "win-down $(($playlist_position - 1))" win-activate "vol ${left}% ${right}%"
