c5a68ecd09fb554ecf8ddf8b678f814e41f09a8e
alex-huff <alexhuff@bakahuff.com>
2023-10-17 13:51:31 -0500

    Removed muting in cmus-play-playlist

--- dotfiles/.local/bin/cmus-play-playlist.zsh
+++ dotfiles/.local/bin/cmus-play-playlist.zsh
@@ -18,18 +18,18 @@ fi
 # the only song in the first dummy playlist should have this in its filename
 identifier=ba304144-e8ad-42c2-9975-93e2c6d0ee56
 
-# go to playlist view, go to top of current window, mute and activate selected item while looking for $identifier in status
-cmus-remote -C "view 3" win-top mute win-activate status | grep $identifier
+# go to playlist view, go to top of current window, and activate selected item while looking for $identifier in filename
+cmus-remote -C "view 3" win-top win-activate "format_print %F" | grep $identifier
 
-# return code of grep identifies whether we are in left or right window
+# return code of grep identifies whether we are in the playlist or track window
 # this works since:
-# if we were in left window, we are now playing the only song in the top playlist, which is named $identifier
-# if we were in the right window, we are now playing the top song in our current playlist, which is not named $identifier
+# if we were in the playlist window, we are now playing the only song in the top playlist, which has $identifier in filename
+# if we were in the track window, we are now playing the top song in our current playlist, which does not have $identifier in filename
 if [ $pipestatus[2] -eq 1 ]
 then
-	# we are in right window, so we need to move to top of left first
+	# we are in track window, so we need to move to top of playlist window
 	setup_commands=(win-next win-top)
 fi
 
-# move to selected playlist, activate it, and restore volume
-cmus-remote -C $setup_commands "win-down $playlist_position" win-activate mute
+# move to selected playlist, and activate it
+cmus-remote -C $setup_commands "win-down $playlist_position" win-activate
