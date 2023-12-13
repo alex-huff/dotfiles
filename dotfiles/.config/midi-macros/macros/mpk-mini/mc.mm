bcb8416174b74ec82ebba6018b985cd8d2ac2126
alex-huff <alexhuff@bakahuff.com>
2023-12-13 16:42:37 -0600

    Made active playback loop

--- dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
+++ dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
@@ -2,7 +2,7 @@
 37{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft record save latest"
 38{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft run playbackRecording.py latest.pcr"
 39{c==9} → switch-active-pycraft-recording.sh
-39+38{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft run playbackRecording.py active.pcr"
+39+38{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft run playbackRecording.py active.pcr true"
 D3 MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"→f"{round(CC_VALUE_SCALED(30, 110))}") [BLOCK|DEBOUNCE]→
 {
 	pid=$(focused-pid.sh)
