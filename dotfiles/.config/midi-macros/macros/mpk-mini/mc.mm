af742a75f5cb2fc5e8af5101b55fc92c710d9a81
alex-huff <alexhuff@bakahuff.com>
2023-12-24 08:37:31 -0600

    Updated midi-macros configuration

--- dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
+++ dotfiles/.config/midi-macros/macros/mpk-mini/mc.mm
@@ -1,8 +1,8 @@
 36{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft record start"
 37{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft record save latest"
-38{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft run playbackRecording.py latest.pcr"
+38{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft run playback_recording.py latest.pcr"
 39{c==9} → switch-active-pycraft-recording.sh
-39+38{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft run playbackRecording.py active.pcr true"
+39+38{c==9} → mc-cli --pid $(focused-pid.sh) send chat ".pycraft run playback_recording.py active.pcr true"
 D3 MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"→f"{round(CC_VALUE_SCALED(30, 110))}") [BLOCK|DEBOUNCE]→
 {
 	pid=$(focused-pid.sh)
