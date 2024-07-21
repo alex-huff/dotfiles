1b7938327ad50af0e9485fc00826db910cf32c8f
alex-huff <alexhuff@bakahuff.com>
2024-07-21 08:33:05 -0500

    Added macro for turning off display

--- dotfiles/.config/midi-macros/macros/mpk-mini/global.mm
+++ dotfiles/.config/midi-macros/macros/mpk-mini/global.mm
@@ -6,7 +6,8 @@ C5{v>64} NOTES[1:]("{}"->[-]f"{72 - MIDI}") -> swaymsg move container to workspa
 C5{v<=64} NOTES[1:]("{}"->[-]f"{72 - MIDI}") -> swaymsg workspace {}
 
 # lock/unlock
-[39|43]{c==9} -> $(which kill) -USR1 swaylock || swaylock -c 000000 --font "Victor Mono"
+(43+39){c==9} -> $(which kill) -USR1 swaylock || swaylock -c 000000 --font "Victor Mono"
+(39+43){c==9} -> swaymsg output '$main_display' toggle
 
 # clipboard manager
 B4{v>64} NOTES[1:]("{}"->[-]ASPN) -> wl-paste -n > ~/.config/midi-macros/clipboards/{} && echo Saving clipboard to file: {}
