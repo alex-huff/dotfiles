19c46938e178f95b985d00342ed8f127ad41fd14
alex-huff <alexhuff@bakahuff.com>
2024-09-03 16:40:28 -0500

    Fixed race condition where screenshots of visual elements that depended on mouse position would sometimes update based on the new mouse position after slurping before screenshot was captured.

--- dotfiles/screenshots/grimshot-imgur.sh
+++ dotfiles/screenshots/grimshot-imgur.sh
@@ -3,5 +3,20 @@
 cd ~/screenshots
 current_time=$(date +%s%9N)
 image_name="screenshot-${current_time}.png"
-grimshot "$@" $image_name
+if [ $1 = "area" ]
+then
+	temp_file=$(mktemp --suffix .png)
+	grim -l 0 $temp_file
+	swayimg --config='Textinfo.show=no' --fullscreen $temp_file &
+	swayimg_pid=$!
+	region=$(slurp -f %w:%h:%x:%y)
+	if [ $? -ne 0 ]
+	then
+		region='in_w:in_h:0:0'
+	fi
+	kill $swayimg_pid
+	ffmpeg -i $temp_file -vf "crop=$region" $image_name
+else
+	grim $image_name
+fi
 upload-to-imgur.sh $image_name
