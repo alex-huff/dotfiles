890f2f615e0e23a521a27023f36d0f6f0db3d417
alex-huff <alexhuff@bakahuff.com>
2024-09-20 17:35:30 -0500

    Reworked "control panel" overlay to be more generic and cleaned up global.mm

--- dotfiles/.config/midi-macros/macros/mpk-mini/global.mm
+++ dotfiles/.config/midi-macros/macros/mpk-mini/global.mm
@@ -25,9 +25,9 @@ MIDI{STATUS==cc}{CC_FUNCTION==71}("{}"->CC_VALUE_PERCENT) [BLOCK|DEBOUNCE]-> cmu
 # control focused application volume with knob 3
 MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"->f"{CC_VALUE_SCALED(0, 1)}") (python)[BLOCK|DEBOUNCE]->
 {
-    import subprocess
+    from subprocess import check_output
     focused_pid = int(
-        subprocess.check_output(
+        check_output(
             "focused-pid.sh",
             text=True,
             shell=True
@@ -52,7 +52,7 @@ E3 MIDI{STATUS==cc}{CC_FUNCTION==72}(CC_VALUE_PERCENT) [BACKGROUND|INVOCATION_FO
 {
     while true
     do
-        ssh alex@archl zsh -c '"xargs -I {} echo {} > /sys/class/backlight/amdgpu_bl1/brightness"'
+        ssh alex@lap "xargs -n1 echo > /sys/class/backlight/amdgpu_bl1/brightness"
         sleep 30
     done
 }
@@ -71,15 +71,15 @@ C3 MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"->CC_VALUE) [BLOCK|DEBOUNCE]->
 # mpv
 MIDI{STATUS==pb}("{}"->f"{DATA_2_SCALED(-57/7, 10):.2f}") (python)[BLOCK|DEBOUNCE]->
 {
-    import subprocess
+    from subprocess import check_output, run
     import json
     playback_rate = {}
-    focused_pid = int(subprocess.check_output('focused-pid.sh'))
-    speed_command = {'command': ['set_property', 'speed', abs(playback_rate)]}
-    direction_command = {'command': ['set_property', 'play-direction', 'forward' if playback_rate > 0 else 'backward']}
-    display_command = {'command': ['show-text', f'Playback Rate: {playback_rate}']}
-    json_payload = '\n'.join(json.dumps(command) for command in (speed_command, direction_command, display_command))
-    subprocess.run(f"echo '{json_payload}' | socat - $XDG_RUNTIME_DIR/mpv-ipc-{focused_pid}.sock", shell=True)
+    focused_pid = int(check_output("focused-pid.sh"))
+    speed_command = {"command": ["set_property", "speed", abs(playback_rate)]}
+    direction_command = {"command": ["set_property", "play-direction", "forward" if playback_rate > 0 else "backward"]}
+    display_command = {"command": ["show-text", f"Playback Rate: {playback_rate}"]}
+    json_payload = "\n".join(json.dumps(command) for command in (speed_command, direction_command, display_command))
+    run(f"echo '{json_payload}' | socat - $XDG_RUNTIME_DIR/mpv-ipc-{focused_pid}.sock", shell=True)
 }
 36{c==9} -> echo '{"command": ["cycle", "pause"]}' | socat - $XDG_RUNTIME_DIR/mpv-ipc-$(focused-pid.sh).sock
 37{c==9} -> echo '{"command": ["playlist-prev"]}' | socat - $XDG_RUNTIME_DIR/mpv-ipc-$(focused-pid.sh).sock
@@ -103,8 +103,8 @@ MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"->f"{round(CC_VALUE_SCALED(0, 130))}") [BL
 C4 NOTES[0:1](ASPN) (python $MM_SCRIPT)[BACKGROUND|INVOCATION_FORMAT=f"{a}\n"]->
 {
     import os
-    import sys
-    import subprocess
+    from sys import stdin
+    from subprocess import Popen, run, check_output, PIPE, CalledProcessError
     import time
     import threading
     import shutil
@@ -113,7 +113,7 @@ C4 NOTES[0:1](ASPN) (python $MM_SCRIPT)[BACKGROUND|INVOCATION_FORMAT=f"{a}\n"]->
     import obswebsocket
 
     def speak(to_speak):
-        speech_process = subprocess.Popen('speak-it', stdin=subprocess.PIPE, text=True)
+        speech_process = Popen("speak-it", stdin=PIPE, text=True)
         speech_process.stdin.write(to_speak)
         speech_process.stdin.close()
 
@@ -131,83 +131,83 @@ C4 NOTES[0:1](ASPN) (python $MM_SCRIPT)[BACKGROUND|INVOCATION_FORMAT=f"{a}\n"]->
             return replay_save_finished_condition.wait_for(lambda: replay_save_finished, timeout=20)
 
     def get_current_timestamp_in_mpv():
-        json_result = subprocess.check_output('echo \'{"command": ["get_property", "playback-time"]}\' | socat - $XDG_RUNTIME_DIR/mpv-ipc-$(focused-pid.sh).sock', shell=True, text=True)
+        json_result = check_output('echo \'{"command": ["get_property", "playback-time"]}\' | socat - $XDG_RUNTIME_DIR/mpv-ipc-$(focused-pid.sh).sock', shell=True, text=True)
         if not json_result:
             return False, -1
         json_result_object = json.loads(json_result)
-        if json_result_object['error'] != 'success':
+        if json_result_object["error"] != "success":
             return False, -1
         return True, json_result_object["data"]
 
     def open_video_in_mpv(video_path):
-        return subprocess.Popen((mpv_path, '--wayland-app-id=mpv-clipper-dialogue', video_path))
+        return Popen((mpv_path, "--wayland-app-id=mpv-clipper-dialogue", video_path))
 
     def clip_video(video_path, beginning_timestamp, end_timestamp):
         duration = end_timestamp - beginning_timestamp
         if duration < 0:
-            return False, ''
+            return False, ""
         try:
-            clip_name = subprocess.check_output('rofi -dmenu -l 0 -p "clip name"', shell=True, text=True).rstrip()
-        except subprocess.CalledProcessError:
-            return False, ''
-        out_path = os.path.expanduser(f'~/clips/{clip_name}')
-        clipper_process = subprocess.run(('ffmpeg', '-y', '-ss', str(beginning_timestamp), '-i', video_path, '-t', str(duration), '-c:a', 'copy', '-c:v', 'copy', out_path))
+            clip_name = check_output('rofi -dmenu -l 0 -p "clip name"', shell=True, text=True).rstrip()
+        except CalledProcessError:
+            return False, ""
+        out_path = os.path.expanduser(f"~/clips/{clip_name}")
+        clipper_process = run(("ffmpeg", "-y", "-ss", str(beginning_timestamp), "-i", video_path, "-t", str(duration), "-c:a", "copy", "-c:v", "copy", out_path))
         return clipper_process.returncode == 0, out_path
 
     def on_replay_saved(event):
         global replay_video_path, replay_save_finished
         with replay_save_finished_condition:
-            replay_video_path = event.datain['savedReplayPath']
+            replay_video_path = event.datain["savedReplayPath"]
             replay_save_finished = True
             replay_save_finished_condition.notify_all()
 
-    websocket_host = 'localhost'
+    websocket_host = "localhost"
     websocket_port = 4455
-    websocket_password = os.environ['OBS_WEBSOCKET_PASSWORD']
+    websocket_password = os.environ["OBS_WEBSOCKET_PASSWORD"]
     websocket = obswebsocket.obsws(websocket_host, websocket_port, websocket_password, authreconnect=5)
     websocket.register(on_replay_saved, obswebsocket.events.ReplayBufferSaved)
-    mpv_path = shutil.which('mpv')
+    mpv_path = shutil.which("mpv")
     currently_clipping = False
     beginning_timestamp = end_timestamp = replay_video_path = clipping_mpv_process = None
     replay_save_finished = False
     replay_save_finished_condition = threading.Condition()
     websocket.connect()
-    for line in sys.stdin:
+    for line in stdin:
         line = line.rstrip()
         match line:
-            case '':
+            case "":
                 if currently_clipping:
                     clipping_mpv_process.kill()
                     clipping_mpv_process.wait()
                     if None in (beginning_timestamp, end_timestamp):
-                        speak('cancelling')
+                        speak("cancelling")
                     else:
-                        speak('saving')
+                        speak("saving")
                         succeeded, clip_video_path = clip_video(replay_video_path, beginning_timestamp, end_timestamp)
-                        speak(f'clipping {"succeeded" if succeeded else "failed"}')
+                        speak(f"clipping {'succeeded' if succeeded else 'failed'}")
                         if succeeded:
                             open_video_in_mpv(clip_video_path)
                     beginning_timestamp = end_timestamp = None
                 else:
                     succeeded = save_obs_replay()
                     if not succeeded:
-                        speak('failed to save replay')
+                        speak("failed to save replay")
                         continue
                     clipping_mpv_process = open_video_in_mpv(replay_video_path)
                 currently_clipping = not currently_clipping
-            case 'B3' | 'D4':
+            case "B3" | "D4":
                 if not currently_clipping:
                     continue
-                currently_setting_beginning_timestamp = line == 'B3'
+                currently_setting_beginning_timestamp = line == "B3"
                 succeeded, timestamp = get_current_timestamp_in_mpv()
                 if succeeded:
                     if currently_setting_beginning_timestamp:
                         beginning_timestamp = timestamp
                     else:
                         end_timestamp = timestamp
-                    speak(f'set clip {"beginning" if currently_setting_beginning_timestamp else "end"}')
+                    speak(f"set clip {'beginning' if currently_setting_beginning_timestamp else 'end'}")
                 else:
-                    speak('could not retrieve timestamp')
+                    speak("could not retrieve timestamp")
             case _:
                 pass
 }
@@ -224,7 +224,7 @@ C4 NOTES[0:1](ASPN) (python $MM_SCRIPT)[BACKGROUND|INVOCATION_FORMAT=f"{a}\n"]->
         new_state=on
     fi
     echo $new_state > $power_state_file
-    echo $(
+    : $(
         for light in $(seq 5)
         do
             hass-cli state turn_${new_state} light.clc${light} &
@@ -238,24 +238,50 @@ C4 NOTES[0:1](ASPN) (python $MM_SCRIPT)[BACKGROUND|INVOCATION_FORMAT=f"{a}\n"]->
 (40+42){c==9} -> hass-cli state toggle switch.out1_mss110_main_channel
 (40+43){c==9} -> hass-cli state toggle switch.out2_mss110_main_channel
 # MIDI{STATUS==cc}{CC_FUNCTION==73}("{}"->f"{CC_VALUE_SCALED(0, 1)}") [BLOCK|DEBOUNCE]-> hass-cli service call --arguments entity_id=media_player.samsung_the_frame_55,volume_level={} media_player.volume_set
-MIDI{STATUS==cc}{CC_FUNCTION==74}("{}"->f"{round(CC_VALUE_SCALED(0, 255))}") [BLOCK|DEBOUNCE|LOCK=light]->
+MIDI{STATUS==cc}{74<=CC_FUNCTION<=76}(
+    f"last_action_time = {TIME}; color[{CC_FUNCTION - 74}] = {round(CC_VALUE_SCALED(0, 255))}\n"
+)
+(python $MM_SCRIPT)[BACKGROUND]->
 {
-    echo {} > ~/.config/midi-macros/state/light-r
-    ~/.config/midi-macros/scripts/update-lights.bash
-}
-MIDI{STATUS==cc}{CC_FUNCTION==75}("{}"->f"{round(CC_VALUE_SCALED(0, 255))}") [BLOCK|DEBOUNCE|LOCK=light]->
-{
-    echo {} > ~/.config/midi-macros/state/light-g
-    ~/.config/midi-macros/scripts/update-lights.bash
-}
-MIDI{STATUS==cc}{CC_FUNCTION==76}("{}"->f"{round(CC_VALUE_SCALED(0, 255))}") [BLOCK|DEBOUNCE|LOCK=light]->
-{
-    echo {} > ~/.config/midi-macros/state/light-b
-    ~/.config/midi-macros/scripts/update-lights.bash
+    from sys import stdin
+    from threading import Thread, Condition
+    from subprocess import Popen, DEVNULL
+
+    def do_hass_update():
+        last_processed_action_time = None
+        while True:
+            with new_action_condition:
+                new_action_condition.wait_for(lambda: last_action_time != last_processed_action_time)
+                color_json = f"[{', '.join(str(component) for component in color)}]"
+                last_processed_action_time = last_action_time
+            processes = [
+                Popen(
+                    (
+                        "hass-cli",
+                        "raw",
+                        "ws",
+                        "--json",
+                        '{"domain":"light","service":"turn_on","service_data":{"entity_id":"light.clc%s","rgb_color":%s}}' % (light, color_json),
+                        "call_service"
+                    ),
+                    stdout=DEVNULL
+                ) for light in range(1, 6)
+            ]
+            for process in processes:
+                process.wait()
+
+    last_action_time = None
+    new_action_condition = Condition()
+    Thread(target=do_hass_update, daemon=True).start()
+    color = [0, 0, 0]
+    for line in stdin:
+        with new_action_condition:
+            exec(line)
+            new_action_condition.notify_all()
 }
 MIDI{STATUS==cc}{CC_FUNCTION==73}("{}"->f"{round(CC_VALUE_SCALED(2000, 6500))}") [BLOCK|DEBOUNCE]->
 {
-    echo $(
+    : $(
         for light in $(seq 5)
         do
             hass-cli service call --arguments "entity_id=light.clc${light},kelvin={}" light.turn_on &
@@ -264,7 +290,7 @@ MIDI{STATUS==cc}{CC_FUNCTION==73}("{}"->f"{round(CC_VALUE_SCALED(2000, 6500))}")
 }
 MIDI{STATUS==cc}{CC_FUNCTION==77}("{}"->f"{round(CC_VALUE_SCALED(0, 255))}") [BLOCK|DEBOUNCE]->
 {
-    echo $(
+    : $(
         for light in $(seq 5)
         do
             hass-cli service call --arguments "entity_id=light.clc${light},brightness={}" light.turn_on &
@@ -272,56 +298,37 @@ MIDI{STATUS==cc}{CC_FUNCTION==77}("{}"->f"{round(CC_VALUE_SCALED(0, 255))}") [BL
     )
 }
 
-# control panel
-MIDI{STATUS==cc}{CC_FUNCTION==74}("{}"->CC_VALUE_PERCENT) [BLOCK|DEBOUNCE|LOCK=eww_light]->
+# knobs
+* MIDI{STATUS==cc}{70<=CC_FUNCTION<=77}(
+    f"last_action_time = {TIME / 10 ** 9}; knob_states[{CC_FUNCTION - 70}] = {CC_VALUE_PERCENT}\n"
+)
+(python $MM_SCRIPT)[BACKGROUND]->
 {
-    eww update light-r={}
-    echo {} > ~/.config/midi-macros/state/eww-slider-r
-    ~/.config/midi-macros/scripts/update-lights-eww.py
-}
-MIDI{STATUS==cc}{CC_FUNCTION==75}("{}"->CC_VALUE_PERCENT) [BLOCK|DEBOUNCE|LOCK=eww_light]->
-{
-    eww update light-g={}
-    echo {} > ~/.config/midi-macros/state/eww-slider-g
-    ~/.config/midi-macros/scripts/update-lights-eww.py
-}
-MIDI{STATUS==cc}{CC_FUNCTION==76}("{}"->CC_VALUE_PERCENT) [BLOCK|DEBOUNCE|LOCK=eww_light]->
-{
-    eww update light-b={}
-    echo {} > ~/.config/midi-macros/state/eww-slider-b
-    ~/.config/midi-macros/scripts/update-lights-eww.py
-}
-MIDI{STATUS==cc}{CC_FUNCTION==70}("{}"->CC_VALUE_PERCENT) [BLOCK|DEBOUNCE]-> eww update main-volume={}
-MIDI{STATUS==cc}{CC_FUNCTION==71}("{}"->CC_VALUE_PERCENT) [BLOCK|DEBOUNCE]-> eww update cmus-volume={}
-MIDI{STATUS==cc}{CC_FUNCTION==72}("{}"->CC_VALUE_PERCENT) [BLOCK|DEBOUNCE]-> eww update focused-volume={}
-MIDI{STATUS==cc}{CC_FUNCTION==73}("{}"->CC_VALUE_PERCENT) [BLOCK|DEBOUNCE]-> eww update temperature={}
-MIDI{STATUS==cc}{CC_FUNCTION==77}("{}"->CC_VALUE_PERCENT) [BLOCK|DEBOUNCE]-> eww update brightness={}
-MIDI{STATUS==cc}{70<=CC_FUNCTION<=77}(TIME) (python $MM_SCRIPT)[BACKGROUND|INVOCATION_FORMAT=f"{a}\n"]->
-{
-    import time
+    from time import time, sleep
+    from sys import stdin
     from threading import Thread, Condition
-    from subprocess import run, check_output, DEVNULL
-    def update_control_panel(is_open):
-        run(f'eww {"open" if is_open else "close"} control-panel-window', stdout=DEVNULL, shell=True)
-    def control_panel_open():
-        return "*" in check_output("eww list-windows | grep control-panel-window", shell=True, text=True)
-    def seconds_to_nanos(time_sec):
-        return time_sec * 10**9
-    def nanos_to_seconds(time_ns):
-        return time_ns / 10**9
-    def sleep_till(time_ns):
-        total_sleep_time = time_ns - time.time_ns()
+    from subprocess import run, DEVNULL
+
+    def update_knobs_window(is_open):
+        run(f"eww {'open' if is_open else 'close'} knobs-window", stdout=DEVNULL, shell=True)
+
+    def is_knobs_window_open():
+        return run("eww active-windows | grep knobs-window", stdout=DEVNULL, shell=True).returncode == 0
+
+    def sleep_till(time_seconds):
+        total_sleep_time = time_seconds - time()
         if total_sleep_time > 0:
-            time.sleep(nanos_to_seconds(total_sleep_time))
+            sleep(total_sleep_time)
+
     def do_hover():
         last_processed_action_time = None
         while True:
             with new_action_condition:
                 new_action_condition.wait_for(lambda: last_action_time != last_processed_action_time)
                 last_processed_action_time = last_action_time
-            was_control_panel_open = control_panel_open()
-            if not was_control_panel_open:
-                update_control_panel(True)
+            was_knobs_window_open = is_knobs_window_open()
+            if not was_knobs_window_open:
+                update_knobs_window(True)
             sleep_till(last_processed_action_time + hover_time)
             while True:
                 with new_action_condition:
@@ -329,15 +336,26 @@ MIDI{STATUS==cc}{70<=CC_FUNCTION<=77}(TIME) (python $MM_SCRIPT)[BACKGROUND|INVOC
                         break
                     last_processed_action_time = last_action_time
                 sleep_till(last_processed_action_time + hover_time)
-            if not was_control_panel_open:
-                update_control_panel(False)
-    hover_time = seconds_to_nanos(1)
+            if not was_knobs_window_open:
+                update_knobs_window(False)
+
+    def do_update_eww_knobs():
+        last_processed_action_time = None
+        while True:
+            with new_action_condition:
+                new_action_condition.wait_for(lambda: last_action_time != last_processed_action_time)
+                variables_string = " ".join(f"knob-{i + 1}={state}" for i, state in enumerate(knob_states))
+                last_processed_action_time = last_action_time
+            run(f"eww update {variables_string}", stdout=DEVNULL, shell=True)
+
+    hover_time = 1
     last_action_time = None
     new_action_condition = Condition()
     Thread(target=do_hover, daemon=True).start()
-    while True:
-        action_time = int(input())
+    Thread(target=do_update_eww_knobs, daemon=True).start()
+    knob_states = [0 for _ in range(8)]
+    for line in stdin:
         with new_action_condition:
-            last_action_time = action_time
-            new_action_condition.notify()
+            exec(line)
+            new_action_condition.notify_all()
 }
