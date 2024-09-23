fbbdb65b6242c97266eac1c34caa03c9829bb0e2
alex-huff <alexhuff@bakahuff.com>
2024-09-23 12:37:55 -0500

    Parallelized twitch-online-filter.py

--- dotfiles/.local/bin/twitch-online-filter.py
+++ dotfiles/.local/bin/twitch-online-filter.py
@@ -1,7 +1,8 @@
 #!/bin/python
 
 import sys
-from itertools import batched, chain
+from itertools import batched, compress, chain
+from multiprocessing.dummy import Pool
 import requests
 
 ENDPOINT = "https://gql.twitch.tv/gql"
@@ -16,28 +17,43 @@ query {
 """
 TWITCH_CLIENT_ID = "kimne78kx3ncx6brgo4mv6wki5h1ko"
 BATCH_SIZE = 35
+MAX_CONCURRENT_REQUESTS = 4
 
-usernames = list(map(str.rstrip, sys.stdin))
-multi_queries = [
-    [
-        {"query": STREAM_ID_QUERY % username, "variables": {}}
-        for username in username_batch
+
+def filter_online_channels_over_stdin():
+    channel_names = list(map(str.rstrip, sys.stdin))
+    multi_queries = [
+        [
+            {"query": STREAM_ID_QUERY % channel, "variables": {}}
+            for channel in channel_batch
+        ]
+        for channel_batch in batched(channel_names, n=BATCH_SIZE)
     ]
-    for username_batch in batched(usernames, n=BATCH_SIZE)
-]
-responses = [
-    requests.post(
-        ENDPOINT,
-        json=multi_query,
-        headers={"client-id": TWITCH_CLIENT_ID},
-    ).json()
-    for multi_query in multi_queries
-]
-query_responses = chain.from_iterable(responses)
-users = (query_response["data"]["user"] for query_response in query_responses)
-stream_datas = (user["stream"] if user != None else False for user in users)
-online = "\n".join(
-    username for username, stream_data in zip(usernames, stream_datas) if stream_data
-)
-if online:
-    print(online)
+    thread_pool = Pool(MAX_CONCURRENT_REQUESTS)
+    try:
+        responses = thread_pool.map(
+            lambda multi_query: requests.post(
+                ENDPOINT, json=multi_query, headers={"client-id": TWITCH_CLIENT_ID}
+            ).json(),
+            multi_queries,
+        )
+    finally:
+        thread_pool.close()
+        thread_pool.join()
+    channels = map(
+        lambda query_response: query_response["data"]["user"],
+        chain.from_iterable(responses),
+    )
+    online_selector = map(
+        lambda channel: True if channel != None and channel["stream"] else False,
+        channels,
+    )
+    return list(compress(channel_names, online_selector))
+
+
+try:
+    online_channels = filter_online_channels_over_stdin()
+    if online_channels:
+        print("\n".join(online_channels))
+except:
+    print("Failed to filter channels", file=sys.stderr)
