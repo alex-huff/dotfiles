05f65fbc48ef5db6f587f57ec4f4ba72b93bd888
alex-huff <alexhuff@bakahuff.com>
2025-06-28 21:04:47 -0500

    file-chooser: source .bash_profile

--- dotfiles/.local/bin/file-chooser.sh
+++ dotfiles/.local/bin/file-chooser.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-CHOICE=$5 RECOMMENDED_SAVE_PATH=$4 CHOOSER='xargs -d "\n" realpath >> "$CHOICE"' kitty --app-id=kitty-dialogue
+CHOICE=$5 RECOMMENDED_SAVE_PATH=$4 CHOOSER='xargs -d "\n" realpath >> "$CHOICE"' kitty --app-id=kitty-dialogue bash --login
