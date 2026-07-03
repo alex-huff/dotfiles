628ed461ec90b37da988ef11ca93271f2bd32c85
alex-huff <alexhuff@bakahuff.com>
2026-07-03 08:44:50 -0500

    totp.sh: tabs > spaces

--- dotfiles/.local/bin/totp.sh
+++ dotfiles/.local/bin/totp.sh
@@ -3,6 +3,6 @@
 twofa_dir=~/.2fa
 choosen_service=$(find $twofa_dir -type f -regex ".*\.key\.gpg" | sed "s|${twofa_dir}/\(.*\)\.key\.gpg|\1|" | fuzzel --dmenu)
 fuzzel --dmenu --password --prompt-only="password: " --width=20 |
-	gpg --passphrase-fd=0 --pinentry-mode=loopback --batch --decrypt --no-symkey-cache ${twofa_dir}/"${choosen_service}".key.gpg |
-		oathtool --base32 --totp - |
-			wl-copy -n
+    gpg --passphrase-fd=0 --pinentry-mode=loopback --batch --decrypt --no-symkey-cache ${twofa_dir}/"${choosen_service}".key.gpg |
+        oathtool --base32 --totp - |
+            wl-copy -n
