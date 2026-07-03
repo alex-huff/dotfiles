b80fc16c64a810d9e4a7037463f864bff97db17e
alex-huff <alexhuff@bakahuff.com>
2026-07-03 08:44:18 -0500

    add-totp-method-from-current-screen.sh: use fuzzel for password entry

--- dotfiles/.local/bin/add-totp-method-from-current-screen.sh
+++ dotfiles/.local/bin/add-totp-method-from-current-screen.sh
@@ -6,18 +6,27 @@ then
     exit 1
 fi
 google_authenticator_json_data=$(echo $uri | extract-google-authenticator-params-from-uris.py | jq ".[0]")
-proto=$(echo $google_authenticator_json_data | jq -r ".proto")
-label=$(echo $google_authenticator_json_data | jq -r ".label")
+proto=$(echo $google_authenticator_json_data | jq --raw-output ".proto")
+label=$(echo $google_authenticator_json_data | jq --raw-output ".label")
 if [ "$proto" != "totp" -o -z "$label" ]
 then
     exit 1
 fi
-secret=$(echo $google_authenticator_json_data | jq -r ".parameters.secret[0]")
+secret=$(echo $google_authenticator_json_data | jq --raw-output ".parameters.secret[0]")
 if [ -z "$secret" ]
 then
     exit 1
 fi
 twofa_dir=~/.2fa
 twofa_key_file=$twofa_dir/"$label".key.gpg
-echo $secret | gpg --yes --output "$twofa_key_file" --symmetric -
-echo $secret | oathtool --base32 --totp - | wl-copy -n
+echo $secret |
+    gpg --yes \
+        --output="$twofa_key_file" \
+        --passphrase-file=<(fuzzel --dmenu --password --prompt-only="password: " --width=20) \
+        --pinentry-mode=loopback \
+        --batch \
+        --symmetric \
+        -
+echo $secret |
+    oathtool --base32 --totp - |
+        wl-copy -n
