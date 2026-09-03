b238e593d8faa0e95e939a0c47dde697b4fb3c4f
alex-huff <alexhuff@bakahuff.com>
2026-09-03 09:41:24 -0500

    kitty-chooser: fix when stdin/stdout is a regular file

--- dotfiles/.local/bin/twitch.sh
+++ dotfiles/.local/bin/twitch.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 export FZF_DEFAULT_OPTS="
-    $FZF_DEFAULT_OPTS
-    --layout=reverse
-    --multi
-    --read0
-    --delimiter='\n'
-    --with-nth='{1}'
-    --accept-nth='{1}'
-    --preview-window='right,70%'
-    --preview='twitch-channel-fzf-preview.sh {2} {3} {4}'"
+$FZF_DEFAULT_OPTS
+--layout=reverse
+--multi
+--read0
+--delimiter='\n'
+--with-nth='{1}'
+--accept-nth='{1}'
+--preview-window='right,70%'
+--preview='twitch-channel-fzf-preview.sh {2} {3} {4}'"
 
 jq_build_fzf_item_script=$(
 	cat <<-"EOF"
